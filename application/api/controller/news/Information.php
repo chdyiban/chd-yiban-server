d1d22ec55ea7a365bb042879697a0b986076c09f
dettac <345098184@qq.com>
2018-05-26 20:40:58 +0800

    增加cms模块并重写部分小程序接口

--- application/api/controller/news/Information.php
+++ application/api/controller/news/Information.php
@@ -6,6 +6,11 @@ use app\common\controller\Api;
 use think\Config;
 use fast\Http;
 
+use addons\cms\model\Archives as ArchivesModel;
+use addons\cms\model\Channel;
+use addons\cms\model\Comment;
+use addons\cms\model\Modelx;
+
 
 /**
  * 资讯栏目控制器
@@ -16,78 +21,73 @@ class Information extends Api
     protected $noNeedRight = ['*'];
 
     public function index(){
-        $page = $this->request->post('page');
-        $openid = $this->request->post('openid');
+        $page = (int)$this->request->get('page');
+        $openid = $this->request->get('openid');
+
+        $params = [];
+        $model = (int)$this->request->request('model');
+        $channel = (int)$this->request->request('channel');
+
+        if ($model) {
+            $params['model'] = $model;
+        }
+        if ($channel) {
+            $params['channel'] = $channel;
+        }
+        $page = max(1, $page);
+        $params['limit'] = ($page - 1) * 10 . ',10';
+
+        $list = ArchivesModel::getArchivesList($params);
+        //$list = ArchivesModel::getWeAppArchivesList($params);
 
         $info = [
             'status' => 200,
             'message' => 'success',
-            'data' => [
-                [
-                    'articleid' => '1',
-                    'type' => 'all',
-                    'style' => '0',
-                    'tag' => '头条',
-                    'title'=>'关于头条的测试情况',
-                    'time'=> '2018-04-07 19:00:00',
-                    'views' => '123',
-                    'likes' => '5',
-                ],
-                [
-                    'articleid' => '2',
-                    'type' => 'new',
-                    'style' => '0',
-                    'tag' => '测试2',
-                    'title'=>'这里如果突然来了一个很长很长很长很长的title，不知道是不是可以显示完全',
-                    'time'=> '2018-04-07 20:00:00',
-                    'views' => '3.5k',
-                    'likes' => '75',
-                ],
-                [
-                    'articleid' => '2',
-                    'type' => 'new',
-                    'style' => '1',
-                    'tag' => '测试2',
-                    'title'=>'这里有一个大图，我将何去何从？',
-                    'time'=> '2018-04-07 20:00:00',
-                    'views' => '3.5k',
-                    'icons' => [
-                        'http://www.chd.edu.cn/_upload/article/images/cb/1e/a1222683457fb6b039a49d82093a/81815730-7ba9-4666-a918-e102c2eb033b.jpg',
-                    ],
-                    'likes' => '75',
-                ],
-                [
-                    'articleid' => '2',
-                    'type' => 'new',
-                    'style' => '2',
-                    'tag' => '测试2',
-                    'title'=>'常规普通模式',
-                    'time'=> '2018-04-07 20:00:00',
-                    'summary' => '这里其实可以显示一些简介，如果可以有这个功能的话，前期可以不提取。',
-                    'views' => '3.5k',
-                    'icons' => [
-                        'http://www.chd.edu.cn/_upload/article/images/e8/bd/a561655346b6881ab48ca323662d/78678899-9674-4845-8304-f1edf4ac0850.jpg',
-                    ],
-                    'likes' => '75',
-                ],
-                [
-                    'articleid' => '2',
-                    'type' => 'new',
-                    'style' => '3',
-                    'tag' => '三图模式',
-                    'title'=>'三图模式的新闻',
-                    'time'=> '2018-04-07 20:00:00',
-                    'summary' => '这里其实可以显示一些简介，如果可以有这个功能的话，前期可以不提取。',
-                    'views' => '3.5k',
-                    'icons' => [
-                        'http://news.chd.edu.cn/_upload/article/images/59/86/f686906f4e80928139e1776b449d/4ff673c1-78e0-4dc1-b218-dedf3c21f548.png',
-                        'http://www.chd.edu.cn/_upload/article/images/e8/bd/a561655346b6881ab48ca323662d/78678899-9674-4845-8304-f1edf4ac0850.jpg',
-                        'http://www.chd.edu.cn/_upload/article/images/e8/bd/a561655346b6881ab48ca323662d/78678899-9674-4845-8304-f1edf4ac0850.jpg',
-                    ],
-                    'likes' => '75',
-                ],
-            ]
+            'data' => $list
         ];
+      
+        return json($info);
+    }
+
+    //对应CMS模块下的新闻详情
+    public function detail(){
+        // $action = $this->request->post("action");
+        // if ($action && $this->request->isPost()) {
+        //     return $this->$action();
+        // }
+        $diyname = $this->request->param('diyname');
+        if ($diyname && !is_numeric($diyname)) {
+            $archives = ArchivesModel::getByDiyname($diyname);
+        } else {
+            $id = $diyname ? $diyname : $this->request->request('id', '');
+            $archives = ArchivesModel::get($id);
+        }
+        if (!$archives || $archives['status'] == 'hidden' || $archives['deletetime']) {
+            $this->error(__('No specified article found'));
+        }
+        $channel = Channel::get($archives['channel_id']);
+        if (!$channel) {
+            $this->error(__('No specified channel found'));
+        }
+        $model = Modelx::get($channel['model_id']);
+        if (!$model) {
+            $this->error(__('No specified model found'));
+        }
+        $archives->setInc("views", 1);
+        $addon = db($model['table'])->where('id', $archives['id'])->find();
+        if ($addon) {
+            $archives = array_merge($archives->toArray(), $addon);
+        }
+
+        $commentList = Comment::getCommentList(['aid' => $archives['id']]);
+
+        $list = ['archivesInfo' => $archives, 'channelInfo' => $channel, 'commentList' => $commentList->getCollection()];
+        $info = [
+            'status' => 200,
+            'message' => 'success',
+            'data' => $list
+        ];
+
         return json($info);
     }
 }
--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,12 +4,26 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
+    'addon_after_upgrade' => 
+    array (
+      0 => 'cms',
+    ),
   ),
   'route' => 
   array (
+    '/cms$' => 'cms/index/index',
+    '/cms/c/[:diyname]' => 'cms/channel/index',
+    '/cms/t/[:name]' => 'cms/tags/index',
+    '/cms/a/[:diyname]' => 'cms/archives/index',
+    '/cms/p/[:diyname]' => 'cms/page/index',
+    '/cms/s' => 'cms/search/index',
+    '/cms/wxapp' => 'cms/wxapp.index/index',
     '/example$' => 'example/index/index',
     '/example/d/[:name]' => 'example/demo/index',
     '/example/d1/[:name]' => 'example/demo/demo1',
     '/example/d2/[:name]' => 'example/demo/demo2',
+    '/third$' => 'third/index/index',
+    '/third/connect/[:platform]' => 'third/index/connect',
+    '/third/callback/[:platform]' => 'third/index/callback',
   ),
 );
