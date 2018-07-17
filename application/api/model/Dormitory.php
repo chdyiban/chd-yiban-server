3547000d584bc0eceb89137884796edbe0b0433a
liutao <282813637@qq.com>
2018-07-17 11:46:56 +0800

    对于家庭人口数为1的进行处理

--- application/api/model/Dormitory.php
+++ application/api/model/Dormitory.php
@@ -145,7 +145,7 @@ class Dormitory extends Model
                 break;
             //需要床号
             case 'bed':
-                if (empty($key['dormitory'])) {
+                if (empty($key['dormitory'] ||empty($key['building']))) {
                     return ['status' => false, 'msg' => "参数有误", 'data' => null];
                 }else{
                     $building = $key['building'];
@@ -233,49 +233,59 @@ class Dormitory extends Model
             return ['status' => false, 'msg' => "信息已经完善", 'data' => null];
         } else {
             $ZCYF = '';
-            foreach ($key['JJDC'][7] as $k => $v) {
-                $ZCYF = $k == 0 ? $v:$ZCYF.",".$v;
+            if (empty($key['JJDC'][7])) {
+                return ['status' => false, 'msg' => "数据有误", 'data' => null];
+            } else{
+                foreach ($key['JJDC'][7] as $k => $v) {
+                    $ZCYF = $k == 0 ? $v:$ZCYF.",".$v;
+                }
             }
             if ($key['JTRKS'] == 0) {
-                return ['status' => false, 'msg' => "数据提供有问题", 'data' => null];
+                return ['status' => false, 'msg' => "数据提供有误", 'data' => null];
             } else {
                 $RJSR = $key['ZSR']/$key['JTRKS'];
             }
-            $data = array(
-                'XH' => $info['stu_id'],
-                'RXQHK' => $key['RXQHK'],
-                'JTRKS' => $key['JTRKS'],
-                'YZBM' => $key['YZBM'],
-                'SZDQ' => $key['SZDQ'],
-                'XXDZ' => $key['XXDZ'],
-                'BRDH' => $key['BRDH'],
-                'ZP' => $key['ZP'][0]['url'],
-                'ZSR' => $key['ZSR'],
-                'RJSR' => $RJSR,
-                'FQZY' => $key['JJDC'][0][0],
-                'MQZY' => $key['JJDC'][1][0],
-                'FQLDNL' => $key['JJDC'][2][0],
-                'MQLDNL' => $key['JJDC'][3][0],
-                'YLZC' => $key['JJDC'][4][0],
-                'SZQK' => $key['JJDC'][5][0],
-                'JTBG' => $key['JJDC'][6][0],
-                'ZCYF' => $ZCYF,
-            );
-        
-        // $res = Db::name('fresh_info_add') -> insert($data);
-            foreach ($key['JTRK'] as $k => $v) {
-                $family_info = array(
-                    'XH' => $info['stu_id'],
-                    'XM' => $v['name'],
-                    'NL' => $v['age'],
-                    'GX' => $v['relation'],
-                    'GZDW' => $v['unit'],
-                    'ZY' => $v['job'],
-                    'NSR' => $v['income'],
-                    'JKZK' => $v['health'],
-                    'LXDH' => $v['mobile'],
-                );
-                $info_family[] = $family_info;
+            $data['XH'] = $info['stu_id'];
+            $data['RXQHK'] = !empty($key['RXQHK']) ? $key['RXQHK']:null;
+            $data['JTRKS'] = !empty($key['JTRKS']) ? $key['JTRKS']:null;
+            $data['YZBM'] = !empty($key['YZBM']) ? $key['YZBM']:null;
+            $data['SZDQ'] = !empty($key['SZDQ']) ? $key['SZDQ'] : null;
+            $data['XXDZ'] = !empty($key['XXDZ']) ? $key['XXDZ'] : null;
+            $data['BRDH'] = !empty($key['BRDH'])?$key['BRDH']:null;
+            $data['ZP'] =  !empty($key['ZP'][0]['url']) ? $key['ZP'][0]['url'] : '';
+            $data['ZSR'] = $key['ZSR'];
+            $data['RJSR'] = $RJSR;
+            if (empty($key['JJDC'][0]) ||empty($key['JJDC'][1]) ||empty($key['JJDC'][2]) ||empty($key['JJDC'][3]) ||empty($key['JJDC'][4]) ||empty($key['JJDC'][5]) ||empty($key['JJDC'][6]) ) {
+                return ['status' => false, 'msg' => "数据提供有误", 'data' => null];
+            } else {
+                $data['FQZY'] = !empty($key['JJDC'][0][0]);
+                $data['MQZY'] = $key['JJDC'][1][0];
+                $data['FQLDNL'] = $key['JJDC'][2][0];
+                $data['MQLDNL'] = $key['JJDC'][3][0];
+                $data['YLZC'] = $key['JJDC'][4][0];
+                $data['SZQK'] = $key['JJDC'][5][0];
+                $data['JTBG'] = $key['JJDC'][6][0];
+                $data['ZCYF'] = $ZCYF; 
+            }    
+            if (empty($key['JTRK']) || empty($key['JTRK'][0]) ) {
+                $family_info = array();
+                $info_family = array();
+            } else {
+                foreach ($key['JTRK'] as $k => $v) {
+                    $info_family = array(); 
+                    $family_info = array(
+                        'XH' => $info['stu_id'],
+                        'XM' => $v['name'],
+                        'NL' => $v['age'],
+                        'GX' => $v['relation'],
+                        'GZDW' => $v['unit'],
+                        'ZY' => $v['job'],
+                        'NSR' => $v['income'],
+                        'JKZK' => $v['health'],
+                        'LXDH' => $v['mobile'],
+                    );
+                    $info_family[] = $family_info;
+                }
             }
             return ['status' => true, 'msg' => "返回成功", 'data' => $data, 'info' => $info_family];
         }
