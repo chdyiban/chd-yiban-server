959db96bb9b1ca94118b9f904b0cf33e0bcf8861
liutao <282813637@qq.com>
2018-08-02 19:10:01 +0800

    修复部分错误

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -20,20 +20,6 @@ class Index extends Api
      */
     public function index()
     {
-        $array = array(
-            'XH' => '2018900001',
-            'SFZH' => '270734',
-        );
-        dump(base64_encode(json_encode($array)));
-        $college_id = 2400;
-        $sex = 1;
-        $data = DB::name('fresh_dormitory') -> where('YXDM',$college_id)
-                -> where('XB', $sex)
-                -> group('LH')
-                -> field('LH')
-                -> select();
-        //$data = array_values($data);
-        dump($data);
         $this->success('请求成功');
     }
 
