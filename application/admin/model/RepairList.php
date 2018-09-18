9dc355875013903ffd248803d31c841a6048a6ad
liutao <282813637@qq.com>
2018-09-18 23:05:03 +0800

    维修后台升级版本

--- application/admin/model/RepairList.php
+++ application/admin/model/RepairList.php
@@ -25,7 +25,7 @@ class RepairList extends Model
     
     public function getStatusList()
     {
-        return ['waited' => __('Status waited'),'accepted' => __('Status accepted'),'dispatched' => __('Status dispatched'),'finished' => __('Status finished'),'refused' => __('Status refused')];
+        return ['waited' => __('Status waited'),'accepted' => __('Status accepted'),'distributed' => __('Status distributed'),'dispatched' => __('Status dispatched'),'finished' => __('Status finished'),'refused' => __('Status refused')];
     }     
 
 
@@ -44,10 +44,15 @@ class RepairList extends Model
     public function gettype(){
         return $this->belongsTo('RepairType', 'service_id');
     }
+    
+    //获取报修类型
+    public function gettypename(){
+        return $this->belongsTo('RepairType', 'specific_id');
+    }
 
-    //获取工人名称
-    public function getworker(){
-        return $this->belongsTo('RepairWorker', 'dispatched_id');
+    //获取分配的单位的名称
+    public function getcompany(){
+        return $this->belongsTo('Admin', 'distributed_id');
     }
 
     //获取地址名称
@@ -55,6 +60,19 @@ class RepairList extends Model
         return $this->belongsTo('RepairAreas', 'address_id');
     }
 
+    //处理数据用来显示
+    public function redata($row){
+        // $time = time();
+        // $res = $this->where('id ='.$ids)->update(['status' => 'accepted', 'admin_id'=> $admin_id]);
+        // $this->where('id = '.$ids)->update(['accepted_time' => $time]);
+        $row['accepted_time'] = date('Y-m-d H:i:s',$row['accepted_time']);
+        $row['distributed_time'] = date('Y-m-d H:i:s',$row['distributed_time']);
+        $row['dispatched_time'] = date('Y-m-d H:i:s',$row['dispatched_time']);
+        $row['finished_time'] = date('Y-m-d H:i:s',$row['finished_time']);
+        $row['refused_time'] = date('Y-m-d H:i:s',$row['refused_time']);
+        return $row;
+    }
+   
     //受理报修内容
     public function accept($ids, $admin_id){
         //这里应该做上可以批量处理
@@ -65,18 +83,13 @@ class RepairList extends Model
         // }
         return $res;
     }
-    //处理数据用来显示
-    public function redata($row){
-        // $time = time();
-        // $res = $this->where('id ='.$ids)->update(['status' => 'accepted', 'admin_id'=> $admin_id]);
-        // $this->where('id = '.$ids)->update(['accepted_time' => $time]);
-        $row['accepted_time'] = date('Y-m-d H:i:s',$row['accepted_time']);
-        $row['dispatched_time'] = date('Y-m-d H:i:s',$row['dispatched_time']);
-        $row['finished_time'] = date('Y-m-d H:i:s',$row['finished_time']);
-        $row['refused_time'] = date('Y-m-d H:i:s',$row['refused_time']);
-        return $row;
+    //分配单位
+    public function distribute($ids,$company_id){
+        $time = time();
+        $res = $this->where('id ='.$ids)->update(['status' => 'distributed', 'distributed_id' => $company_id]);
+        $this->where('id = '.$ids)->update(['distributed_time' => $time]);
+        return $res;
     }
-
     //指派工人
     public function dispatch($ids,$worker_id){
         $time = time();
@@ -95,15 +108,21 @@ class RepairList extends Model
 
     //报修内容已完工
     public function finish($ids){
-        //这里应该做上可以批量处理
-        // foreach($ids as $id){
-            $time = time();
-            $res = $this->where('id ='.$ids)->update(['status' => 'finished']);
-            $this->where('id = '.$ids)->update(['finished_time' => $time]);
-        // }
-        return $res;
+        $time = time();
+        $res = $this->where('id ='.$ids)->update(['status' => 'finished']);
+        $this->where('id = '.$ids)->update(['finished_time' => $time]);
+        $data = Db::view('repair_list') 
+                -> view('repair_worker','id,name','repair_list.dispatched_id = repair_worker.id')
+                -> view('admin','id,nickname','repair_list.distributed_id = admin.id')
+                -> where('repair_list.id',$ids)
+                -> field('repair_list.id,repair_list.dispatched_id,finished_time,repair_list.distributed_id') 
+                -> find();
+        $info['repair_id'] = $data['id'];
+        $info['worker_name'] = $data['name'];
+        $info['distributed_name'] = $data['nickname'];
+        $info['finished_time'] = $data['finished_time'];
+        $result = Db::name('repair_log') -> insert($info);
+        return $result;
     }
-
-
-
+    
 }
