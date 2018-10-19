f2bf45313b4dab5c156ce9ab7ff36f188a896f4c
liutao <282813637@qq.com>
2018-10-19 18:47:30 +0800

    取消新生绑定易班方法注释

--- application/api/controller/yiban/Ids.php
+++ application/api/controller/yiban/Ids.php
@@ -24,58 +24,56 @@ class Ids extends Api
             }
         }
     }
-    /**
-     * 此api失效已经注册完毕防止信息泄露
-     * 
-     */
-    // public function register(){
-    //     $stu_id = $this -> request -> param('XH');
-    //     if (empty($stu_id)) {
-    //         $this -> error('参数非法');
-    //     } else {
-    //         $result = $this -> getphone($stu_id);
-    //         if (!$result['status']) {
-    //             $this -> error($result['msg'],$result['data']);
-    //         } else {
-    //             $phone = $result['data'];
-    //             $info = array();
-    //             $data = Db::view('fresh_info') 
-    //                     -> view('dict_college','YXJC,yb_group_id','fresh_info.YXDM = dict_college.YXDM')
-    //                     -> where('XH',$stu_id) 
-    //                     -> field('ID,XH,XM,XBDM,SYD')
-    //                     -> find();
-    //             $info['name'] = $data['XM'];
-    //             $info['student_id'] = $data['XH'];
-    //             $info['role'] = '0';
-    //             $info['build_time'] = time();
-    //             $info['status'] = '0';
-    //             $info['schooling'] = '4';
-    //             $info['education'] = '0';
-    //             $info['sex'] = $data['XBDM'];
-    //             $info['college'] = $data['yb_group_id'];
-    //             $info['phone'] = $phone;
-    //             $info['enter_year'] = '2018';
-    //             $info['specialty'] = '';
-    //             $info['eclass'] = '';
-    //             $info['native_place'] = $data['SYD'];
-    //             $info['instructor_id'] = '';
-    //             $this -> success('获取成功',$info);
-    //         }
-    //     }
-    // }
+    
+
+    public function register(){
+        $stu_id = $this -> request -> param('XH');
+        if (empty($stu_id)) {
+            $this -> error('参数非法');
+        } else {
+            $result = $this -> getphone($stu_id);
+            if (!$result['status']) {
+                $this -> error($result['msg'],$result['data']);
+            } else {
+                $phone = $result['data'];
+                $info = array();
+                $data = Db::view('fresh_info') 
+                        -> view('dict_college','YXJC,yb_group_id','fresh_info.YXDM = dict_college.YXDM')
+                        -> where('XH',$stu_id) 
+                        -> field('ID,XH,XM,XBDM,SYD')
+                        -> find();
+                $info['name'] = $data['XM'];
+                $info['student_id'] = $data['XH'];
+                $info['role'] = '0';
+                $info['build_time'] = time();
+                $info['status'] = '0';
+                $info['schooling'] = '4';
+                $info['education'] = '0';
+                $info['sex'] = $data['XBDM'];
+                $info['college'] = $data['yb_group_id'];
+                $info['phone'] = $phone;
+                $info['enter_year'] = '2018';
+                $info['specialty'] = '';
+                $info['eclass'] = '';
+                $info['native_place'] = $data['SYD'];
+                $info['instructor_id'] = '';
+                $this -> success('获取成功',$info);
+            }
+        }
+    }
     /**
      * 获取联系方式
      * @param int stu_id
      * @return array
      */
-    // private function getphone($stu_id){
-    //     $phone = Db::name('fresh_info_add') -> where('XH', $stu_id) -> field('BRDH') -> find();
-    //     if (empty($phone)) {
-    //         return ['status' => false, 'msg' => '请先在选宿舍系统填写调查问卷' , 'data' => null];
-    //     } else {
-    //         return ['status' => true, 'msg' => '查询成功' , 'data' => $phone['BRDH']];
-    //     }
-    // }
+    private function getphone($stu_id){
+        $phone = Db::name('fresh_info_add') -> where('XH', $stu_id) -> field('BRDH') -> find();
+        if (empty($phone)) {
+            return ['status' => false, 'msg' => '请先在选宿舍系统填写调查问卷' , 'data' => null];
+        } else {
+            return ['status' => true, 'msg' => '查询成功' , 'data' => $phone['BRDH']];
+        }
+    }
 
 
 }
