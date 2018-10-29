cd5c534c8042756c2bfbe19d9a142a4581b9c603
liutao <282813637@qq.com>
2018-10-29 21:22:45 +0800

    新闻修改时间以及初步提交小程序码接口

---
+++ application/api/controller/Wxcode.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\api\controller;
+
+use app\common\controller\Api;
+use think\Config;
+use fast\Http;
+use think\Db;
+use \think\Cache;
+use app\api\model\Wxuser as WxuserModel;
+
+
+/**
+ * 获取微信小程序码的接口
+ */
+class Wxcode extends Api
+{
+
+    protected $noNeedLogin = ['*'];
+    protected $noNeedRight = ['*'];
+
+    const GET_ACCESS_TOKEN_URL = 'https://api.weixin.qq.com/cgi-bin/token';
+    const GET_CODE_URL = 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token=';
+
+    public function getAccessToken()
+    {
+        $appid = Config::get('wx.appId');
+        $appsecret = Config::get('wx.appSecret');
+        $param = [
+            'grant_type' => 'client_credential',
+            'appid'      => $appid,
+            'secret'     => $appsecret,
+        ];
+        $response = Http::get($this::GET_ACCESS_TOKEN_URL,$param);
+        $response = json_decode($response,true);
+        if (empty($response['errcode']) || $response['errcode'] == 0) {
+            //将结果写入缓存
+            Cache::set('WxAccessToken', $response['access_token'],$response['expires_in']);
+            return $response['access_token'];
+        } else {
+            $this -> error($response['errmsg']);
+        }
+    }
+
+    public function getWXACodeUnlimit()
+    {
+        header('Content-type:image/jpeg'); 
+        //判断缓存是否有access_token
+        $access_token = Cache::get('WxAccessToken');
+        if (empty($access_token)) {
+             $access_token = $this->getAccessToken();
+
+        }
+        $scene = $this->request->get('scene');
+        //$page = $this->request->get('page');
+        if (empty($scene)) {
+            $this->error('参数有误');
+        }
+        $auto_color = false;
+        $is_hyaline = false;
+        $param = [
+            'scene'        => $scene,
+        ];
+        //dump($access_token);
+        $postData = json_encode($param);
+        $response = Http::post(self::GET_CODE_URL.$access_token,$postData);
+        return $response;
+    }
+
+}
