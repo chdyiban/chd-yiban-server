d3822f01a6a74b296791497d70f77ce0ed48e44d
liutao <282813637@qq.com>
2020-08-17 21:08:25 +0800

    选宿舍2020初次提交

---
+++ application/api/model/dormitory2020/User.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace app\api\model\dormitory2020;
+
+use think\Model;
+use think\Db;
+
+class User extends Model
+{
+    // 表名
+    protected $name = 'fresh_info';
+    
+    /**
+     * 获取用户相关信息
+     * @param string param["XH"]
+     * @return array
+     * 5次SQL
+     */
+    public function getMeInfo($param)
+    {
+        $result = [
+            "user_info" => [],
+            "user_step" => [],
+            "dormitory" => [],
+        ];
+        if (empty($param["XH"])) {
+            return ["status" => false,"msg" =>"param error!","data" => null];
+        }
+        
+        $result["user_info"] = Db::view("fresh_info","XM,XH,YXDM,ZYMC,XBDM,LXDH,QQ,avatar,SYD,BJDM")
+                            -> view("dict_college","YXDM,YXMC","fresh_info.YXDM = dict_college.YXDM")
+                            -> where("fresh_info.XH",$param["XH"])
+                            -> find();
+        //标记床位
+        $markList = Db::name("fresh_mark")->field("SSDM,CH")->where("XH",$param["XH"])->find();
+        $result["user_info"]["BJCW"] = empty($markList) ? "" : $markList["SSDM"]."-".$markList["CH"]; 
+        $nowStep = $this->getSteps($param);
+        $nowData = $this->getStepData($nowStep,$param["XH"]);
+        $nowData["now"] = $nowStep;
+        // $result["user_step"] = ["now" => $nowStep,"data" => $nowData];
+        $result["user_step"] = $nowData;
+        //读取配置文件决定选宿舍状态，迁移至基类getstep方法下
+        $result["dormitory"] = $param["step"];
+
+        return ["status" => true, "msg" => null, "data" => $result];
+    }
+
+    /**
+     * 修改用户头像
+     * @param string avatar
+     * @return array 
+     */
+    public function avatar($param,$userId)
+    {
+        if (empty($param["avatar"])) {
+            return ["status" => false,"msg" =>"param error!","data" => null];
+        }
+        $response = $this->where("ID",$userId) -> update(["avatar" => $param["avatar"]]);
+        if ($response) {
+            return ["status" => true,"msg" =>"更换成功","data" => ["avatar"=>$param["avatar"]]];
+        } else {
+            return ["status" => true,"msg" =>"更换失败，请稍后重试","data" => null];
+        }
+    }
+
+
+    /**
+     * 修改用户QQ
+     * @param string qq
+     * @return array 
+     */
+    public function qq($param,$userId)
+    {
+        if (empty($param["qq"])) {
+            return ["status" => false,"msg" =>"param error!","data" => null];
+        }
+        $response = $this->where("ID",$userId) -> update(["QQ" => $param["qq"]]);
+        return ["status" => true,"msg" =>"提交成功","data" => ["qq"=>$param["qq"]]];
+
+    }
+
+    /**
+     * 获取用户当前步骤
+     * @param string XH
+     * @return string
+     */
+
+    private function getSteps($param){
+        //判断信息是否完善
+        $userXh = $param["XH"];
+        $isInfoExist = Db::name('fresh_questionnaire_base') -> where('XH', $userXh) -> field('ID,XH') -> find();
+        $isListExist = Db::name('fresh_result') -> where('XH', $userXh) -> field('ID,XH,status') -> find();
+        if (empty($isInfoExist)) {
+            return 'QUE';
+        } elseif ($param["step"]["step"] == "NST"){
+            return 'SEL';
+        } elseif (empty($isListExist)) {
+            return 'SEL';
+        } elseif ($isListExist['status'] == "waited"){
+            return "CON";
+        } else {
+            return "FIN";
+        }
+        
+    }
+
+    /**
+     * 获取当前用户步骤附带数据
+     * @param string step
+     * @param int XH
+     * @return array
+     */
+    private function getStepData($step,$XH)
+    {
+        switch ($step) {
+            case 'CON':
+                $returnList = [];
+                $selectList = Db::name("fresh_result")->where("XH",$XH)->field("YXDM,XH,SSDM,CH,XQ,SDSJ")->find();
+                if (!empty($selectList)) {
+                    $returnList = [
+                        "XH"       => $selectList["XH"],
+                        "XQ"       => $selectList["XQ"] == "north" ? "渭水" : "雁塔",
+                        "building" => explode("#",$selectList["SSDM"])[0],
+                        "room"     => explode("#",$selectList["SSDM"])[1],
+                        "bed"      => $selectList["CH"],
+                        "deadline" => date("Y-m-d H:i:s",$selectList["SDSJ"]+3600 ),
+                    ];
+                }
+
+                $dormitoryInfo = Db::name("fresh_dormitory_".$selectList["XQ"])
+                                ->where("SSDM",$selectList["SSDM"])
+                                ->where("YXDM",$selectList["YXDM"])
+                                ->field("CPXZ")
+                                ->find();
+                $bedCount = strlen($dormitoryInfo["CPXZ"]);
+                $returnList["cost"] = $bedCount == 4 ? 1200 : 900; 
+                return $returnList;
+                
+            case "FIN":
+                $returnList = [];
+                $selectList = Db::name("fresh_result")->where("XH",$XH)->field("YXDM,XH,SSDM,CH,XQ")->find();
+                if (!empty($selectList)) {
+                    $returnList = [
+                        "XH"       => $selectList["XH"],
+                        "XQ"       => $selectList["XQ"] == "north" ? "渭水" : "雁塔",
+                        "building" => explode("#",$selectList["SSDM"])[0],
+                        "room"     => explode("#",$selectList["SSDM"])[1],
+                        "bed"      => $selectList["CH"],
+                    ];
+                }
+
+                $dormitoryInfo = Db::name("fresh_dormitory_".$selectList["XQ"])
+                                ->where("SSDM",$selectList["SSDM"])
+                                ->where("YXDM",$selectList["YXDM"])
+                                ->field("CPXZ")
+                                ->find();
+                $bedCount = strlen($dormitoryInfo["CPXZ"]);
+                $returnList["cost"] = $bedCount == 4 ? 1200 : 900; 
+                return $returnList;
+            
+            default:
+                # code...
+                break;
+        }
+    }
+    
+}
---
+++ application/api/model/dormitory2020/Wxuser.php
@@ -0,0 +1,324 @@
+<?php
+namespace app\api\model\dormitory2020;
+
+
+use think\Model;
+use think\Db;
+use think\Cache;
+use think\Config;
+use fast\Http;
+
+
+class Wxuser extends Model {
+    // 表名
+
+    protected $name = 'wx_unionid_user';
+
+
+    const GET_ACCESS_TOKEN_URL = "https://api.weixin.qq.com/sns/oauth2/access_token";
+    const GET_USERINFO_URL = "https://api.weixin.qq.com/sns/userinfo";
+    const TEST_URL = "http://202.117.64.236:8080/auth/login";
+    /**
+     * 初始化方法，判断用户状态
+     * @param string code
+     * @return array 
+     */
+    public function initStep($code)
+    {
+        $result = $this->getOpenId($code);
+        if ($result["status"] == false) {
+            return ["status" => false ,"msg" => $result["msg"] ];
+        }
+        //第一次请求获取用户openid以及access_token信息
+        $accessInfo = $result["data"];
+        $returnArray    =   [
+            "is_bind"           =>  false,  //用户是否绑定门户账号
+            "is_bind_mobile"    =>  false,  //用户是否绑定手机号
+        ];
+        $infoList = $this->where("open_id",$accessInfo["openid"])->find();
+        //获取数据库用户的详细信息
+        $userInfoData = $this->getUserInfo($accessInfo);
+        if ($userInfoData["status"] == false) {
+            return ["status" => false,"msg" => $userInfoData["msg"]];
+        }
+        $userInfo = $userInfoData["data"];
+
+        foreach ($accessInfo as $key => $value) {
+            $userInfo[$key] = $value;
+        }
+
+        if (empty($infoList)) {
+            //用户未曾登录，则使用户先授权，将用户基本信息放入数据库
+            $userId = $this->insertInfo($userInfo);
+        } else {
+            $userId = $this->updateInfo($userInfo);
+        }
+        if (empty($infoList["portal_id"])) {
+            $returnArray["is_bind"] = false;
+        }
+        $checkUnionid = Db::name("fresh_info")->where("XH",$infoList["portal_id"])->field("unionid")->find();
+        if(empty($checkUnionid["unionid"]) || $checkUnionid["unionid"] != $infoList["unionid"] ){
+            $returnArray["is_bind"] = false;
+            $result = Db::name("wx_unionid_user")
+                        ->where("unionid",$infoList["unionid"])
+                        ->update(["portal_id" => ""]);
+        } else {
+            $returnArray["is_bind"] = true;
+        }
+        $returnArray["is_bind_mobile"] = !empty($infoList["mobile"]) ? true : false;
+        $returnArray["open_id"] = $accessInfo["openid"];
+        $returnArray["user_info"] = [
+            "nickname"  =>  $userInfo["nickname"],
+            "avatar"    =>  $userInfo["headimgurl"],
+        ];
+        if(!empty($infoList["portal_id"])) {
+            $returnArray["user_info"]["portal_id"] = $infoList["portal_id"];
+        }
+       
+
+        return ["status" => true,"data" => $returnArray];
+    }
+  
+    /**
+     * 获取用户openid以及access_token信息
+     * @param string $code
+     * @return array 
+     */
+    private function getOpenId($code)
+    {
+        $appid = Config::get('wechat.yibanchd')["appId"];
+        $appsecret = Config::get('wechat.yibanchd')["appSecret"];
+        $retData = [];
+        $params = [
+            'appid' => $appid,
+            'secret' => $appsecret,
+            'code' => $code,
+            'grant_type' => 'authorization_code'
+        ];
+        
+        $result = json_decode(Http::get(self::GET_ACCESS_TOKEN_URL, $params),true);
+        if (empty($result["openid"])) {
+            return ["status" => false,"msg" => $result["errmsg"] ];
+        }
+        return ["status" => true, "data" => $result];
+    }
+
+    /**
+     * 获取用户信息userinfo
+     * @param $params
+     * @return array
+     */
+    private function getUserInfo($params)
+    {
+      
+        $access_token = $params["access_token"];
+        $openid    =   $params["openid"];
+        $params = [
+            "access_token"  =>  $access_token,
+            "openid"        =>  $openid,
+            "lang"          =>  "zh_CN",
+        ];
+        $userInfo = json_decode(Http::get(self::GET_USERINFO_URL, $params),true);
+        if (!empty($userInfo["errmsg"])) {
+            return ["status" => false, "msg" => $userInfo["errmsg"]];
+        }
+        return ["status" => true, "data" => $userInfo];
+    }
+
+    /**
+     * 将用户所有信息插入数据库
+     * @return int  返回用户新增id
+     */
+    private function insertInfo($params)
+    {
+        //首先判断小程序表中是否有门户信息
+        // $portalInfo = Db::name("wx_user")->where("unionid",$params["unionid"])->find();
+        $portal_id  =   "";
+        $portal_pwd =   "";
+        // if (!empty($portalInfo["portal_id"]) && !empty($portalInfo["portal_pwd"]) ) {
+        //     $portal_id = $portalInfo["portal_id"];
+        //     $portal_pwd = _token_decrypt($portalInfo["portal_pwd"], $portalInfo['open_id']);
+        //     $portal_pwd = _token_encrypt($portal_pwd, $params['openid']);
+        // }
+        $insertData = [
+            "open_id"           =>  $params["openid"],
+            "unionid"           =>  $params["unionid"],
+            "create_time"       =>  time(),
+            "last_visit_time"   =>  time(),
+            "portal_id"         =>  $portal_id,
+            "portal_pwd"        =>  $portal_pwd,
+            "access_token"      =>  $params["access_token"],
+            "refresh_token"     =>  $params["refresh_token"],
+            "expires_time"      =>  $params["expires_in"],
+            "avatar"            =>  $params['headimgurl'],
+            "nickname"          =>  $params["nickname"],
+            "mobile"            =>  "",
+            "update_time"       =>  time(),
+        ];
+        $result = Db::name("wx_unionid_user")->insertGetId($insertData);
+        return $result;
+    }
+
+    /**
+     * 将用户access_token信息更新数据库
+     * @return int 返回用户id
+     */
+    private function updateInfo($params)
+    {
+        $userId = Db::name("wx_unionid_user")->where("open_id",$params["openid"])->find()["id"];
+        $updateData = [
+            "last_visit_time"   =>  time(),
+            "access_token"      =>  $params["access_token"],
+            "refresh_token"     =>  $params["refresh_token"],
+            "expires_time"      =>  $params["expires_in"],
+            "avatar"            =>  !empty($params['headimgurl']) ?$params['headimgurl'] :"",
+            "nickname"          =>  $params["nickname"],
+            "update_time"       =>  time(),
+        ];  
+        $result = Db::name("wx_unionid_user")
+                ->where("open_id",$params["openid"])
+                ->update($updateData);
+        return $userId;
+    }
+
+    /**
+     * 将用户门户信息保存至数据库
+     */
+    public function bind($params)
+    {
+        $mobile = "";
+        $params["portal_pwd"] = _token_encrypt($params["portal_pwd"], $params['unionid']);
+        $check = Db::name("wx_unionid_user")->where("unionid",$params["unionid"])->find();
+        if (!empty($check["portal_id"])) {
+            return ["status" => false,"msg" => "请勿重复绑定"];
+        }
+        //判断wx_base表中是否有学号信息记录
+        $check = Db::name("wx_base")->where("portal_id",$params["portal_id"])->find();
+        if (empty($check)) {
+            $this->insertBase($params);
+        }
+        if (!empty($check["mobile"])) {
+            $mobile = $check["mobile"];
+        }
+        $result =  Db::name("wx_unionid_user")
+                    -> where("unionid",$params["unionid"])
+                    -> update([
+                        "portal_id"     =>  $params["portal_id"],
+                        "portal_pwd"    =>  $params["portal_pwd"],
+                        "mobile"        =>  $mobile,
+                    ]);
+        if ($result) {
+            return ["status" => true,"msg" => "绑定成功"];
+        }
+        return ["status" => false,"msg" => "请勿重复绑定"];
+    }
+    /**
+     * 向wx_base中插入信息
+     * @param $params["portal_pwd"]通过unionID加密
+     */
+    private function insertBase($params)
+    {
+        $insertData = [
+            "portal_id" =>  $params["portal_id"],
+            "mobile"    =>  "",
+            "yb_user_id"=>  "",
+        ];
+        $result = Db::name("wx_base")->insert($insertData);
+    }
+
+    /**
+     * 取消门户绑定
+     */
+    public function bindCancel($params)
+    {
+        if (empty($params["unionid"])) {
+            return ["status" => false,"msg" => "请勿重复取消！"];
+        }
+        $check = Db::name("wx_unionid_user")->where("unionid",$params["unionid"])->find();
+        if (empty($check["portal_id"])) {
+            return ["status" => false,"msg" => "请勿重复取消！"];
+        }
+        $updateFlag = false;
+        $deleteFlag = false;
+        Db::startTrans();
+        try{   
+            $updateFlag =  Db::name("wx_unionid_user")
+                            -> where("unionid",$params["unionid"])
+                            -> update([
+                                "portal_id"     =>  "",
+                                // "portal_pwd"    =>  "",
+                            ]);
+            $deleteFlag = Db::name("fresh_info")
+                        -> where("XH",$params["XH"])
+                        -> update(["unionid"=>""]);
+            //提交事务
+            Db::commit();      
+        } catch (\Exception $e) {
+            // 回滚事务
+            Db::rollback();
+        }    
+      
+        if ($updateFlag&&$deleteFlag) {
+            return ["status" => true,"msg" => "取消成功"];
+        }
+        return ["status" => false,"msg" => "请稍后再试"];
+    }
+
+    /**
+     * 验证用户的账号密码是否正确
+     */
+    public function checkBind($params){
+        $username = $params["portal_id"];
+        $password = $params["portal_pwd"];
+        $post_data = [
+            'userName' => $username,
+            'pwd' => $password,
+        ];
+        $return = [];
+        $response = Http::post(self::TEST_URL,$post_data);
+        $response = json_decode($response,true);
+        return $response['success'];
+    }
+
+     /**
+     * 获取新生ID验证账号密码正确性
+     * @param string $user["studentID"]
+     * @param string $user["password"]
+     * @param string $user["open_id"]
+     * @return bool|int 
+     * 
+     */
+    public function check($user){
+        //新生数据库进行比对，若成功则返回userid ，若不成功返回false
+        if (empty($user['studentID']) || empty($user['password']) || empty($user["open_id"]) ) {
+            return false;
+        } else {
+            $info = Db::name("fresh_info")-> where('XH', $user['studentID'])
+                        -> field('SFZH,ID')
+                        -> find(); 
+            if (empty($info)) {
+                return false;
+            } else {
+                $id_card = $info['SFZH'];
+                $password = substr($id_card, -6);
+                if ($user['password'] == $password) {
+                    $userid = $info['ID'];
+                    $res = $this-> where("open_id",$user["open_id"]) 
+                                -> update(["portal_id" => $user['studentID']]);
+
+                    $unionID = $this->where("open_id",$user["open_id"])->field("unionid")->find()["unionid"];
+                    $res = Db::name("fresh_info") -> where("XH",$user["studentID"])
+                                ->update(["unionid"=> $unionID]);
+
+                    if (!$res) {
+                        return -1;
+                    }
+                    return $userid;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+ 
+}
